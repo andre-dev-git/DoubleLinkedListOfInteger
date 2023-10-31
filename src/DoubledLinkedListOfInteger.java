746ca952f3cf0e2711cf4a5b7ebcf72cd60873dd
andre-dev-git <andre.carvalhobjj2002@gmail.com>
2023-10-31 08:29:06 -0300

    Adição do método 'add'

--- src/DoubledLinkedListOfInteger.java
+++ src/DoubledLinkedListOfInteger.java
@@ -14,4 +14,38 @@ public class DoubledLinkedListOfInteger {
             this.next = null;
         }
     } 
+
+    public void add(int index, int data) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+
+        Node newNode = new Node(data);
+
+        if (index == 0) {
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+        } else if (index == size) {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+        size++;
+    }
+    
 }
